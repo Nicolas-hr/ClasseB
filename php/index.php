6923d675f9dca801ebcd5472bda3a390e7637664
TanguyCavagna <tanguy-cvgn@eduge.ch>
2018-05-08 16:09:12 +0200

    Modification des imports et ajouts de la function showProjects())

--- php/index.php
+++ php/index.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: FONSECAF_INFO
- * Date: 08.05.2018
- * Time: 15:55
- */
+require_once './projects.php';
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Classe2B</title>
+</head>
+<body>
+<?php showProjects() ?>
+</body>
+</html>
