1c3324003909cfe276cf1bfb0ea0987d9d8bb29c
TanguyCavagna <tanguy.cvgn@eduge.ch>
2018-05-15 17:39:37 +0200

    Modification du footer pour le rendre plus user-friendly

--- php/footer.php
+++ php/footer.php
@@ -6,3 +6,65 @@
         <p>&copy; I.DA-P2B</p>
     </div>
 </footer>
+<footer class="mainfooter" role="contentinfo">
+    <div class="footer-top p-y-2">
+        <!--  Contact  -->
+    </div>
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Adresse</h4>
+                        <address>
+                            <ul class="list-unstyled">
+                                <li>
+                                    Rue Gérard-de-Ternier<br>
+                                    12<br>
+                                    Petit-Lancy<br>
+                                    1213<br>
+                                </li>
+                                <li>
+                                    Phone: 0
+                                </li>
+                            </ul>
+                        </address>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <!--Column1-->
+
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <!--Column2-->
+
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <!--Column3-->
+                    <div class="footer-pad">
+                        <h4>Informations du site</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Website Tutorial</a></li>
+                            <li><a href="#">Accessibility</a></li>
+                            <li><a href="#">Disclaimer</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Webmaster</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <!--Footer Bottom-->
+                    <p class="text-xs-center">&copy; Copyright 2018 - Classe I.DA/FA-P2B. Tout droits réservés.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
