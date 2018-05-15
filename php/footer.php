14312e1e5b9222f88336ef2347b658ee96e59765
TanguyCavagna <tanguy.cvgn@eduge.ch>
2018-05-15 18:36:34 +0200

    Modif du footer

--- php/footer.php
+++ php/footer.php
@@ -1,11 +1,3 @@
-<footer class="mt-3 text-muted">
-    <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>&copy; I.DA-P2B</p>
-    </div>
-</footer>
 <footer class="mainfooter" role="contentinfo">
     <div class="footer-top p-y-2">
         <!--  Contact  -->
