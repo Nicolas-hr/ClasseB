16d2b0b4cb8dff82575d585e2348962eca40b962
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-15 13:42:25 +0200

    Jai fais la fonction pour upload un projet en .zip

--- php/projects.php
+++ php/projects.php
@@ -15,5 +15,27 @@ function showProjects() {
 
 function uploadProject( )
 {
-  echo "upload projet";
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $projectFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  $nbError = 0;
+
+  $nameFile = filter_input(INPUT_POST, "fileName", FILTER_SANITIZE_STRING);
+
+  $errors = array();
+
+  // Verifie si le fichier est une partition
+  if(isset($_POST["submit"])) {
+    if($projectFileType != "zip") {
+        $errors["extension"] = "Juste les fichier .zip ou les dossier sont acceptés sur ce site.";
+    }
+    // Check if file already exists
+    elseif (file_exists($target_file)) {
+       $errors["fichierExistant"] = "Sorry, file already exists.";
+    }
+  }
+
+  if(count($errors) == 0){
+    header("Location: index.php");
+  }
 }
