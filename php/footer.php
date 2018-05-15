359afec7dd85b44d4b895f10175d408d8ee90922
PtitSoleil <adar-sores.gnr@eduge.ch>
2018-05-15 13:43:48 +0200

    J'ai un peu modifié le footer

--- php/footer.php
+++ php/footer.php
@@ -1,1 +1,6 @@
-<footer>© I.DA-P2B</footer>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<footer class="text-muted">
+	<div class="container">
+        <p>&copy; I.DA-P2B</p>
+    </div>
+</footer>
