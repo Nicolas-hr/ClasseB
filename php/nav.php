547b165b2c2168894c43f4e348469018448695e1
PtitSoleil <adar-sores.gnr@eduge.ch>
2018-05-15 13:16:27 +0200

    J'ai ajouté la navbar

---
+++ php/nav.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Portfolio</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">A Propos</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Inscription</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+    </ul>
+  </div>
+</nav>
