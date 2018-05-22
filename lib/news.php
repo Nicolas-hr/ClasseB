9caba6fbd2bc57d5d2545d4855ce9cef3a864b9f
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-22 15:39:56 +0200

    corriger erreurs

--- lib/news.php
+++ lib/news.php
@@ -4,9 +4,9 @@ require_once '../lib/security.php';
 $titre = filter_input( INPUT_POST, 'titre', FILTER_SANITIZE_STRING);
 $description = filter_input( INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 
-if (!$titre || !$description || strlen($titre) < 2 || strlen($descriptio) < 5 ) {
+if (!$titre || !$description || strlen($titre) < 2 || strlen($description) < 5 ) {
   errorAddNews($titre, $description);
-  include '../newsForm.php';
+  include '../php/newsForm.php';
 }else {
   addNews($titre, $description);
   header('Location: ../php/showNews.php');
