00cf07ee6296e6b5ca932dbb44f229712c1093dd
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-22 14:43:13 +0200

    j'ai fais le lien avec la bd

--- lib/news.php
+++ lib/news.php
@@ -1,8 +1,19 @@
 <?php
 function addNews()
 {
-  // code...
-}
+  try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=classe2b;charset=utf8mb4', 'root');
+  } catch (Exception $e) {
+    die("erreur : ". $e->getMessage());
+  }
+
+  $req = $db->prepare('INSERT INTO tbl_user(Nm_News, Txt_Contents_News) VALUES(:titre, :description)');
+  $salt = uniqid();
+
+  $req->execute(array(
+    'titre' => $titre,
+    'description' => $description
+    ));}
 
 function errorAddNews()
 {
