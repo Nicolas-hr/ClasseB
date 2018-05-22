b77e0cc3f6c2de62596f137d9c7c7923c6eec23a
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-22 15:36:06 +0200

    ajouter fonctionne normalement

--- lib/news.php
+++ lib/news.php
@@ -4,3 +4,11 @@ require_once '../lib/security.php';
 $titre = filter_input( INPUT_POST, 'titre', FILTER_SANITIZE_STRING);
 $description = filter_input( INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 
+if (!$titre || !$description || strlen($titre) < 2 || strlen($descriptio) < 5 ) {
+  errorAddNews($titre, $description);
+  include 'signupForm.php';
+}else {
+  addNews($titre, $description);
+  header('Location: ../php/showNews.php');
+  exit;
+}
