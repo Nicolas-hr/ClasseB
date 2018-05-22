ffc8f654a1361761443549b2d25edf5cafb04eb7
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-22 16:01:24 +0200

    ajouter messages

--- lib/news.php
+++ lib/news.php
@@ -4,8 +4,10 @@ require_once '../lib/security.php';
 $titre = filter_input( INPUT_POST, 'titre', FILTER_SANITIZE_STRING);
 $description = filter_input( INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 
+$message = "";
+
 if (!$titre || !$description || strlen($titre) < 2 || strlen($description) < 5 ) {
-  errorAddNews($titre, $description);
+  errorAddNews($titre, $description, $message);
   include '../php/newsForm.php';
 }else {
   addNews($titre, $description);
