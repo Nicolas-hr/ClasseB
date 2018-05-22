fff01bf4169ebf612689030c19b37c7fec684716
FranciscOliv <francisco.fnscd@eduge.ch>
2018-05-22 14:06:20 +0200

    Je dab sur register.php

--- lib/register.php
+++ lib/register.php
@@ -1,51 +1,122 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-          integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+require_once "../lib/dbConnect.php";
 
-    <!-- Custom styles for this template -->
-    <link href="../css/register.css" rel="stylesheet">
+//VARIALES DANS LA SESSION
+if (!isset($_SESSION['firstNameReg'])) {
+    $_SESSION['firstNameReg'] = '';
+    $_SESSION['lastNameReg'] = '';
+    $_SESSION['emailReg'] = '';
+    $_SESSION['errorReg'] = [
+        'firstName' => '',
+        'lastName' => '',
+        'email' => '',
+        'password' => '',
+    ];
+}
 
-    <title>Document</title>
-</head>
-<body>
+//VARIABLES LOCALES
+$firstName = "";
+$lastName = "";
+$email = "";
+$pwd = "";
+$error = false;
 
-<?php require_once 'nav.php'; ?>
 
-<main class="container">
-    <form class="form-signin text-center">
-        <img class="mb-4 rounded" src="../images/php.jpg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
-</main>
+//Lors du click dans le bouton submit
+if (filter_has_var(INPUT_POST, 'register')) {
 
-<!-- JQUERY - JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-        integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+
+    //Vider le variables de session
+    $_SESSION['firstNameReg'] = '';
+    $_SESSION['lastNameReg'] = '';
+    $_SESSION['emailReg'] = '';
+    $_SESSION['errorReg'] = [
+        'firstName' => '',
+        'lastName' => '',
+        'email' => '',
+        'password' => '',
+    ];
+
+    $error = false;
+
+    //Entrees par l'utilisateur
+    $firstName = filter_input(INPUT_POST, 'firstNameR', FILTER_SANITIZE_EMAIL);
+    $lastName = filter_input(INPUT_POST, 'lastNameR', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'emailR', FILTER_VALIDATE_EMAIL);
+    $pwd = filter_input(INPUT_POST, 'pwdR', FILTER_SANITIZE_STRING);
+
+
+    if (empty($firstName)) {
+        $_SESSION['errorReg']['firstName'] = "Le prénom rentré n'est pas valable";
+        $error = true;
+    }
+    if (empty($lastName)) {
+        $_SESSION['errorReg']['lastNameReg'] = "Le prénom rentré n'est pas valable";
+        $error = true;
+    }
+    if (empty($email)) {
+        $_SESSION['errorReg']['email'] = "L'email rentré n'est pas valable";
+        $error = true;
+    }
+    if (empty($pwd)) {
+        $_SESSION['errorReg']['password'] = "Le mot de passe rentré n'est pas valable";
+        $error = true;
+    }
+    if (!$error) {
+        if (emailVerify($email)) {
+            addUser($firstName, $lastName, $email, $pwd);
+            header("Location:../login.php");
+            exit;
+        } else {
+            $_SESSION['errorReg'] = "L'email n'est pas valable.";
+        }
+    }
+
+
+}
+header("Location:../register.php");
+exit;
+
+//FONCTIONS-------------------------------------------------------------------------------------------------------------
+
+function emailVerify($emailVerify)
+{
+    $db = dbConnect();
+
+    $email_ok = false;
+
+    $emailRequest = $db->prepare("SELECT Txt_Email 'email'  FROM tbl_email WHERE Txt_Email=:email");
+    $emailRequest->execute(array(":email" => $emailVerify));
+
+    if ($emailRequest->rowCount() == 0) {
+        $email_ok = true;
+    }
+    return $email_ok;
+}
+
+function addUser($firstNameUser, $lastNameUser, $emailUser, $pwdUser)
+{
+    $db = dbConnect();
+
+    $salt = uniqid();
+    $pwdSalt = $pwdUser . $salt;
+    $pwdHash = sha1($pwdSalt);
+
+    //Ajout dans la table user
+    $userAddRequest = $db->prepare("INSERT INTO tbl_user(Nm_First, Nm_Last, Txt_Password_Hash, Txt_Password_Salt) VALUES (:firstname,:lastname, :pwdhash, :pwdsalt )");
+    $userAddRequest->execute(array(":firstname" => $firstNameUser, ":lastname" => $lastNameUser, ":pwdhash" => $pwdHash, ":pwdsalt" => $salt));
+
+    //Numero du dernier index de la table user
+    $lastIndex = $db->lastInsertId();
+
+    //Ajout dans la table email
+    $userAddRequest = $db->prepare("INSERT INTO tbl_email(Txt_Email,Id_User) VALUES (:email,:lastUserId )");
+    $userAddRequest->execute(array(":email" => $emailUser, ":lastUserId" => $lastIndex));
+}
+
+
+
