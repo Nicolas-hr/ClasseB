be3872d4adf16c26505e2bed04a32fe72b919067
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-22 15:32:26 +0200

    ou7i

--- lib/news.php
+++ lib/news.php
@@ -1,3 +1,6 @@
 <?php
 require_once '../lib/security.php';
 
+$titre = filter_input( INPUT_POST, 'titre', FILTER_SANITIZE_STRING);
+$description = filter_input( INPUT_POST, 'description', FILTER_SANITIZE_STRING);
+
