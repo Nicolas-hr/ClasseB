5fbb3aa81db92d053ac8355a48eb46ad23f29b06
Nocxxecru <nicolas.hr@eduge.ch>
2018-05-22 15:17:29 +0200

    changer nom d'un lien

--- php/nav.php
+++ php/nav.php
@@ -16,7 +16,7 @@
                 "uploadsForm.php" => "Mise en ligne",
                 "registerForm.php" => "Inscription",
                 "loginForm.php" => "Connexion",
-                "newsForm.php" => "Ajouter des news",
+                "newsForm.php" => "Les news",
                 "profil.php" => "Profil",
             );
 
