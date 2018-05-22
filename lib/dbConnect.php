53e9ba305ce99b43c2b9b65e85716bcaca832fee
FranciscOliv <francisco.fnscd@eduge.ch>
2018-05-22 15:07:12 +0200

    Changement de la base de données dans dbConnect et changement du register

--- lib/dbConnect.php
+++ lib/dbConnect.php
@@ -5,7 +5,7 @@ function dbConnect()
     $servername = "127.0.0.1";
     $username = "root";
 
-    $db = new PDO("mysql:host=$servername;dbname=mydb", $username);
+    $db = new PDO("mysql:host=$servername;dbname=classe2b", $username);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $db->setAttribute(PDO::ATTR_PERSISTENT, true);
