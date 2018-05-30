7365e36f357b578b5d40c755765676c43c811338
Nicolas.hr <38466252+Nocxxecru@users.noreply.github.com>
2018-05-30 10:29:13 +0200

    Retirer lieninutile

--- php/footer.php
+++ php/footer.php
@@ -32,20 +32,7 @@
                     <!--Column2-->
 
                 </div>
-                <div class="col-md-3 col-sm-6">
-                    <!--Column3-->
-                    <div class="footer-pad">
-                        <h4>Informations du site</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Website Tutorial</a></li>
-                            <li><a href="#">Accessibility</a></li>
-                            <li><a href="#">Disclaimer</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">FAQs</a></li>
-                            <li><a href="#">Webmaster</a></li>
-                        </ul>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
