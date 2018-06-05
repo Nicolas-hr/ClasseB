050939472e8d498927429ce1ec5767a75446c420
FranciscOliv <francisco.fnscd@eduge.ch>
2018-06-05 16:13:03 +0200

    dab

--- php/showNews.php
+++ php/showNews.php
@@ -16,6 +16,8 @@ require_once '../lib/security.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+    <title>News</title>
 </head>
 <body class="parallax">
 <!-- Navabar -->
